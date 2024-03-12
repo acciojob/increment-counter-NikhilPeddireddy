408d143e129ae36922e2432aeb8017e6d11664d4
NikhilPeddireddy <139708292+NikhilPeddireddy@users.noreply.github.com>
2024-03-12 17:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let count=0;
+let para=document.getElementById("counter");
+let incBtn=document.getElementById("incrementBtn");
+incBtn.addEventListener("click", ()=>{
+	count++;
+	para.innerText=count;
+})
